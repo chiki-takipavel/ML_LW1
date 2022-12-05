636f9d68f76adeb89c1db790a57b96eab4b99902
Pavel Miskevich <chiki-takipavel@mail.ru>
2022-12-05 22:32:19 +0300

    Some small changes

--- main.py
+++ main.py
@@ -5,10 +5,8 @@ import random
 from matplotlib import pyplot as plt
 import pandas as pd
 import logging
-from typing import List
-from numpy.typing import NDArray
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split, learning_curve
+from sklearn.model_selection import train_test_split
 import datetime
 
 DATASET_FOLDER = r'C:/Users/chiki/Downloads/notMNIST_large'
@@ -17,8 +15,8 @@ DATA_COLUMN_NAME = 'data'
 LABELS_COLUMN_NAME = 'labels'
 HASHED_DATA_COLUMN_NAME = 'data_bytes'
 BALANCE_BORDER = 0.85
-MAX_ITERATIONS_COUNT = 100
-TRAIN_SIZES = [50, 100, 1000, 10000, 50000]
+MAX_ITERATIONS_COUNT = 1000000
+TRAIN_SIZES = [50, 100, 1000, 10000, 50000, 200000]
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
@@ -48,45 +46,53 @@ def get_class_data(folder_path):
     return result_data
 
 
-def is_class_balanced(class_images_count):
-    max_images_count = max(class_images_count)
-    avg_images_count = sum(class_images_count) / len(class_images_count)
+def get_classes_images_counts(data_frame):
+    classes_images_counts = list()
+    for class_index in range(len(CLASSES)):
+        labels = data_frame[LABELS_COLUMN_NAME]
+        class_rows = data_frame[labels == class_index]
+        class_count = len(class_rows)
+
+        classes_images_counts.append(class_count)
+        logging.info(f"Class {CLASSES[class_index]} contains {class_count} images")
+
+    return classes_images_counts
+
+
+def show_classes_histogram(classes_images_counts):
+    plt.figure()
+    plt.bar(CLASSES, classes_images_counts)
+    plt.show()
+    logging.info("Histogram shown")
+
+
+def check_classes_balance(data_frame):
+    classes_images_counts = get_classes_images_counts(data_frame)
+
+    max_images_count = max(classes_images_counts)
+    avg_images_count = sum(classes_images_counts) / len(classes_images_counts)
     balance_percent = avg_images_count / max_images_count
+
+    show_classes_histogram(classes_images_counts)
     logging.info(f"Balance: {balance_percent:.3f}")
+    if balance_percent > BALANCE_BORDER:
+        logging.info("Classes are balanced")
+    else:
+        logging.info("Classes are not balanced")
 
-    return True if balance_percent > BALANCE_BORDER else False
 
-
-def analyze_all_classes():
-    """
-    Analyze all classes:
-
-    1 Check if classes are balanced
-
-    2 Create data frame
-
-    :return: data - list of ndarrays
-    """
-    images_counts_list = list()
+def create_data_frame():
     data = list()
     labels = list()
     for class_item in CLASSES:
         class_folder_path = os.path.join(DATASET_FOLDER, class_item)
         class_data = get_class_data(class_folder_path)
 
-        class_images_count = len(class_data)
-        logging.info(f"{class_item} folder contains {class_images_count} files")
-
-        images_counts_list.append(class_images_count)
         data.extend(class_data)
-        labels.extend([CLASSES.index(class_item) for _ in range(class_images_count)])
-
-    if is_class_balanced(images_counts_list):
-        logging.info("Classes are balanced")
-    else:
-        logging.info("Classes are not balanced")
+        labels.extend([CLASSES.index(class_item) for _ in range(len(class_data))])
 
     data_frame = pd.DataFrame({DATA_COLUMN_NAME: data, LABELS_COLUMN_NAME: labels})
+    logging.info("Data frame is created")
 
     return data_frame
 
@@ -115,33 +121,28 @@ def split_dataset_into_subsamples(data_frame):
 
     x_train, x_remaining, y_train, y_remaining = train_test_split(data, labels, train_size=0.8)
     x_valid, x_test, y_valid, y_test = train_test_split(x_remaining, y_remaining, test_size=0.5)
+    logging.info("Data split")
 
     return x_train, y_train, x_test, y_test, x_valid, y_valid
 
 
-def get_logistic_regression(x_train, y_train, x_test):
-    logistic_regression = LogisticRegression(solver='lbfgs', max_iter=MAX_ITERATIONS_COUNT)
-    logistic_regression.fit(x_train, y_train)
-    logging.info("Regression fit is completed")
+def get_logistic_regression(x_train, y_train, x_test, y_test):
+    test_scores = list()
+    for train_size in TRAIN_SIZES:
+        logistic_regression = LogisticRegression(max_iter=MAX_ITERATIONS_COUNT)
+        logistic_regression.fit(x_train[:train_size], y_train[:train_size])
+        logging.info("Regression fit is completed")
 
-    logistic_regression.predict(x_test)
-    logging.info("Predict is completed")
+        score = logistic_regression.score(x_test, y_test)
+        logging.info("Score is calculated")
+        test_scores.append(score)
 
-    train_sizes, train_scores, test_scores = learning_curve(
-        logistic_regression,
-        x_train,
-        y_train,
-        train_sizes=TRAIN_SIZES
-    )
-
-    return train_sizes, train_scores, test_scores
+    return test_scores
 
 
-def show_result_plot(train_sizes, train_scores, test_scores):
-    train_scores_mean = np.mean(train_scores, axis=1)
-    train_scores_std = np.std(train_scores, axis=1)
-    test_scores_mean = np.mean(test_scores, axis=1)
-    test_scores_std = np.std(test_scores, axis=1)
+def show_result_plot(test_scores):
+    test_scores_mean = np.mean(test_scores)
+    test_scores_std = np.std(test_scores)
 
     plt.figure()
     plt.title('Learning curve')
@@ -149,18 +150,9 @@ def show_result_plot(train_sizes, train_scores, test_scores):
     plt.ylabel('Accuracy')
     plt.grid()
 
-    plt.plot(train_sizes, train_scores_mean, 'o-', color='r', label='Training score')
+    plt.plot(TRAIN_SIZES, test_scores_mean, 'o-', color='g', label='Testing score')
     plt.fill_between(
-        train_sizes,
-        train_scores_mean - train_scores_std,
-        train_scores_mean + train_scores_std,
-        alpha=0.25,
-        color='r'
-    )
-
-    plt.plot(train_sizes, test_scores_mean, 'o-', color='g', label='Training score')
-    plt.fill_between(
-        train_sizes,
+        TRAIN_SIZES,
         test_scores_mean - test_scores_std,
         test_scores_mean + test_scores_std,
         alpha=0.25,
@@ -176,14 +168,15 @@ def main():
 
     show_some_images()
 
-    data_frame = analyze_all_classes()
+    data_frame = create_data_frame()
     data_frame = remove_duplicates(data_frame)
+    check_classes_balance(data_frame)
     data_frame = shuffle_data(data_frame)
 
-    x_train, y_train, x_test, *_ = split_dataset_into_subsamples(data_frame)
+    x_train, y_train, x_test, y_test, *_ = split_dataset_into_subsamples(data_frame)
 
-    train_sizes, train_scores, test_scores = get_logistic_regression(x_train, y_train, x_test)
-    show_result_plot(train_sizes, train_scores, test_scores)
+    test_scores = get_logistic_regression(x_train, y_train, x_test, y_test)
+    show_result_plot(test_scores)
 
     end_time = datetime.datetime.now()
     logging.info(end_time - start_time)
